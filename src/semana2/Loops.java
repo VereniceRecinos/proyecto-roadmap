7c8d5a6f91771f34ee8b430e8ef959452e772fde
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-04-09 17:06:55 -0600

    Semana 2: Uso de bucles for en Java

---
+++ src/semana2/Loops.java
@@ -0,0 +1,17 @@
+package semana2;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Número: " + i);
+		}
+		
+		for (int i = 5; i >= 1; i--) {
+			System.out.println("Cuenta regresiva: " + i);
+		}
+
+	}
+
+}
