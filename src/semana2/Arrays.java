ed3642c568480b83a5bb49dd6ac314d9de34cfac
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-04-09 17:13:43 -0600

    Semana 2: Uso de arrays

---
+++ src/semana2/Arrays.java
@@ -0,0 +1,14 @@
+package semana2;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int [] numeros = {10, 20, 30, 40, 50};
+		
+		System.out.println("Primer número: " + numeros[0]);
+		System.out.println("Tercer número: " + numeros[2]);
+		
+	}
+
+}
