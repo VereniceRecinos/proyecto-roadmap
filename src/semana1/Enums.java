d871fbe4c5ba59446d23658940093fcfd6fadaeb
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-04-08 23:01:34 -0600

    Semana 1: Uso de enums

---
+++ src/semana1/Enums.java
@@ -0,0 +1,16 @@
+package semana1;
+
+enum Dia {
+	LUNES, MARTES, MIERCOLES, JUEVES, VIERNES
+}
+
+public class Enums {
+
+	public static void main(String[] args) {
+		
+		Dia hoy = Dia.LUNES;
+		
+	System.out.println("Hoy es: " + hoy);
+	}
+
+}
