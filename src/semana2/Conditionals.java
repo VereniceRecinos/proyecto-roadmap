433514bf3962ba97cd151cdc78f300dc1f8c46f8
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-04-09 16:59:49 -0600

    Semana 2: Uso de estructuras condicionales

---
+++ src/semana2/Conditionals.java
@@ -0,0 +1,20 @@
+package semana2;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		
+		int nota = 75;
+		
+		if (nota >= 90) {
+			
+			System.out.println("Excelente");
+		} else if (nota >= 60) {
+			System.out.println("Aprobado");
+		} else {
+			System.out.println("Reprobado");
+		}
+
+	}
+
+}
