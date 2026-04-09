94ce115b525bec81334fc8d5029fcfc976be8c2d
Emilyn Verenice Recinos Salguero <erecinoss5@miumg.edu.gt>
2026-04-09 16:40:05 -0600

    Semana 2: Uso de Strings y métodos básicos

---
+++ src/semana2/Strings.java
@@ -0,0 +1,16 @@
+package semana2;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Emilyn";
+		
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Longitud: " + nombre.length());
+		System.out.println("Mayúsculas: " + nombre.toUpperCase());
+		System.out.println("Minúsculas: " + nombre.toLowerCase());
+
+	}
+
+}
